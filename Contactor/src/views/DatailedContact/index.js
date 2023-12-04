a3bfc2ccf2f57743466bf1a533d7da1fb26b6870
valmargutt <valmargutt@gmail.com>
2023-12-04 13:09:34 +0000

    Layout komid og haegt ad opna modal

--- Contactor/src/views/DatailedContact/index.js
+++ Contactor/src/views/DatailedContact/index.js
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Image, Alert } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import ContactDetail from "../../components/ContactDetail";
+import ContactEditModal from "../../components/ContactEditModal";
 
 const DetailedContacts = ({}) => {
-	const navigation = useNavigation();
-	const route = useRoute();
+	const [isContactEditModalOpen, setIsContactEditModalOpen] = useState(false);
+	const [editingContact, setEditingContact] = useState(null);
+	const [currentContact, setCurrentContact] = useState([]);
+
 	// const uuid = route.params?.uuid;
 
 	const editContact = () => {
 		// A function to edit a contact
-		console.log("Edit a contact");
+
+		setIsContactEditModalOpen(true);
 	};
 
 	return (
 		<View style={{ flex: 1 }}>
-			<ContactDetail editContact={editContact} />
+			<ContactDetail
+				editCurrentContact={() => editContact(currentContact)}
+			/>
+			<ContactEditModal
+				isOpen={isContactEditModalOpen}
+				closeModal={() => setIsContactEditModalOpen(false)}
+				contact={editingContact}
+				// onUpdateList={onUpdateList}
+			/>
 		</View>
 	);
 };
