24c2144d2b2c03d194202e8fa4ead81456d57984
LovisaBa <103430711+LovisaBa@users.noreply.github.com>
2023-12-04 16:54:30 +0000

    styling for contactList and contactOverview

--- Contactor/src/components/ContactOverview/index.js
+++ Contactor/src/components/ContactOverview/index.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import styles from "./styles";
 
-const Contact = ({ name, number }) => {
+const Contact = ({ name, number, photo }) => {
 	const { navigate } = useNavigation();
 
 	return (
-		<View style={styles.contact}>
-			<Text>{name}</Text>
-			<Text>{number}</Text>
-			<Text>Photo</Text>
-			{/* <TouchableOpacity onPress={() => navigate("DetailedContacts")}>
-				<Text>Move to detailed contacts</Text>
-			</TouchableOpacity> */}
+		<View style={styles.container}>
+			<TouchableOpacity
+				onPress={() => navigate("DetailedContacts")}
+				style={styles.imageContainer}
+			>
+				<Image source={{ uri: photo }} style={styles.image} />
+				<Text style={styles.name}>{name}</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
-
-const styles = StyleSheet.create({
-	contact: {
-		backgroundColor: "pink",
-		margin: 10,
-	},
-});
 
 export default Contact;
--- Contactor/src/components/Toolbar/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-
-const styles = StyleSheet.create({
-	button: {
-		backgroundColor: "blue",
-		padding: 10,
-		borderRadius: 5,
-	},
-});
