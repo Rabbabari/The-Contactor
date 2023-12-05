f3f347d8eb8f8d53a49220b80e3eda6362a8b1ff
valmargutt <valmargutt@gmail.com>
2023-12-05 09:54:08 +0000

    nafn og mynd kominn inn i detailedcontact

--- Contactor/src/components/ContactDetail/index.js
+++ Contactor/src/components/ContactDetail/index.js
@@ -2,19 +2,15 @@ import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import styles from "./styles";
 
-const ContactDetail = ({ editCurrentContact }) => {
+const ContactDetail = ({ name, phoneNumber, photo, editCurrentContact }) => {
+	console.log(name, phoneNumber, photo);
 	return (
 		<View style={styles.container}>
 			<View style={styles.imageContainer}>
-				<Image
-					source={{
-						uri: "https://photutorial.com/wp-content/uploads/2022/03/Selfies-statistics-thumbnail.png",
-					}}
-					style={styles.image}
-				/>
+				<Image source={{ uri: photo }} style={styles.image} />
 			</View>
-			<Text style={styles.name}>Jane Doe</Text>
-			<Text style={styles.number}>+123 456 7890</Text>
+			<Text style={styles.name}>{name}</Text>
+			<Text style={styles.number}>{phoneNumber}</Text>
 
 			<TouchableOpacity
 				style={styles.button}
